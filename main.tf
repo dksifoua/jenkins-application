9fad82b7f95f0e0a0e0167214dda7e33ef4293de
dksifoua <dimitri.sifoua@gmail.com>
2023-10-20 16:30:01 -0400

    Test terraform workflow

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "jenkins_controller" {
-  ami = "ami-0f34c5ae932e6f0e4"
+  ami           = "ami-0f34c5ae932e6f0e4"
   instance_type = "t2.micro"
-  user_data = file("./httpd.sh")
+  user_data     = file("./httpd.sh")
 }
--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
-  region = "ca-central-1"
+  region  = "ca-central-1"
   profile = "dimitri@dksifoua.io"
 }
